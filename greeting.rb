c38aaa995dc1bb26b09a8811916855151d1d77ff
EC2 Default User <ttsk951031@gmail.com>
2023-11-24 09:50:00 +0000

    条件分岐

---
+++ greeting.rb
@@ -0,0 +1,12 @@
+country = 'italy'
+
+greeting =
+  if country == 'japan'
+    puts 'こんにちは'
+  elsif country == 'us'
+    puts 'Hello'
+  elsif country == 'italy'
+    puts 'Ciao'
+  else
+    '???'
+  end
