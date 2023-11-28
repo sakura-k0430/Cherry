2c98120f1293cabb4b3612811f766c911e8fbe41
EC2 Default User <ttsk951031@gmail.com>
2023-11-28 03:32:14 +0000

    FizzBuzzへのMinitest導入

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,6 +1,6 @@
 def fizz_buzz(n)
   if n % 15 == 0
-    'FizzBizz'
+    'Fizz Buzz'
   elsif n % 3 == 0
     'Fizz'
   elsif n % 5 == 0
@@ -9,11 +9,3 @@ def fizz_buzz(n)
     n.to_s
   end
 end
-
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
